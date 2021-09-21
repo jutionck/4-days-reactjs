337bdf4dbaade6d60e8c6aed693e57f5a1de7d34
Jution Candra Kirana <jutionck@gmail.com>
2021-09-21 23:26:01 +0700

    #2 Props Component

--- README.md
+++ README.md
@@ -6,4 +6,73 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 #### PART Component
 
-> 1. Create component
+> 1. Update `components` directory, create a new directory: `book`
+> 2. Move `BookComponent.js` to `book` directory
+
+Open `BookComponent.js` and then modify like this
+```js
+<Col lg={3} md={6}>
+  <Card className="mt-5">
+      ...
+  </Card>
+</Col>
+```
+
+#### PART Props
+
+Open `App.js` and then modify like this
+```js
+function App() {
+  const books = [
+    {
+      id: 1,
+      title: 'Asesmen Pembelajaran Berbasis Komputer Dan Android',
+      description: 'Implementasi Teknologi Informasi dan Komunikasi (TIK) pada lembaga pendidikan saat ini sudah menjadi keharusan, karena penerapan TIK dapat menjadi salah satu indikator keberhasilan suatu institusi pendidikan. Tidak sedikit dosen yang memanfaatkan kemajuan teknologi tersebut.',
+      image: 'https://cdn.gramedia.com/uploads/items/Asesmen_Pembelajaran_Berbasis_Komputer_dan_Android.jpg'
+    },
+    {
+      id: 2,
+      title: 'Fs Mudah Membuat dan Berbisnis Aplikasi Android dengan Android Studio',
+      description: 'Mudah Membuat dan Berbisnis Aplikasi Android dengan Android Studio Penulis: Yudha Yudhanto & Ardhi Wijayanto Pengembangan aplikasi digital untuk perangkat mobile (ponsel pintar dan tablet) merupakan salah satu bidang yang prospektif untuk terus dikembangkan.',
+      image: 'https://cdn.gramedia.com/uploads/items/ID_MMBAA2018MTH05MMBAA.jpg'
+    },
+    {
+      id: 3,
+      title: 'Pemrograman Web Seri Php: Langkah Mudah Dan Praktis Memahami',
+      description: 'Saat ini, PHP banyak dipakai untuk membuat program situs web dinamis. Contoh aplikasi program PHP adalah forum (phpBB) dan MediaWiki (software di belakang Wikipedia). ',
+      image: 'https://cdn.gramedia.com/uploads/items/Pemrograman_WEb_PHP.jpg'
+    },
+    {
+      id: 4,
+      title: 'Mahir Bahasa Pemrograman PHP',
+      description: 'Dengan pesatnya perkembangan teknologi saat ini, kita dituntut untuk bisa membuat dan mengembangkan sebuah sistem atau program berbasis web. Pembuatan sebuah sistem berbasis web bisa menggunakan berbagai macam bahasa pemrograman. Namun, pada umumnya orang menggunakan bahasa pemrograman PHP.',
+      image: 'https://cdn.gramedia.com/uploads/items/9786020498768_Mahir_Bahasa_.jpg'
+    }
+  ];
+
+  return (
+    <Container>
+      <Row>
+        {
+          books.map((book) =>
+            <BookComponent
+              key={book.id}
+              title={book.title}
+              description={book.description}
+              variant="primary" />
+          )
+        }
+      </Row>
+    </Container>
+  );
+}
+
+```
+
+##### Hands On
+
+> Rapihkan di `App.js` supaya tidak terlihat seperti di atas
+> 
+> Buat sebuah compenent baru dengan nama `Book` dan `BookList` silahkan atur dan sesuaikan
+>
+> Jika sudah, maka di `App.js` yang di panggil adalah component `Boook`
--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import BookComponent from "./components/book/BookComponent";
 
 function App() {
 
+  const books = [
+    {
+      id: 1,
+      title: 'Asesmen Pembelajaran Berbasis Komputer Dan Android',
+      description: 'Implementasi Teknologi Informasi dan Komunikasi (TIK) pada lembaga pendidikan saat ini sudah menjadi keharusan, karena penerapan TIK dapat menjadi salah satu indikator keberhasilan suatu institusi pendidikan. Tidak sedikit dosen yang memanfaatkan kemajuan teknologi tersebut.',
+      image: 'https://cdn.gramedia.com/uploads/items/Asesmen_Pembelajaran_Berbasis_Komputer_dan_Android.jpg'
+    },
+    {
+      id: 2,
+      title: 'Fs Mudah Membuat dan Berbisnis Aplikasi Android dengan Android Studio',
+      description: 'Mudah Membuat dan Berbisnis Aplikasi Android dengan Android Studio Penulis: Yudha Yudhanto & Ardhi Wijayanto Pengembangan aplikasi digital untuk perangkat mobile (ponsel pintar dan tablet) merupakan salah satu bidang yang prospektif untuk terus dikembangkan.',
+      image: 'https://cdn.gramedia.com/uploads/items/ID_MMBAA2018MTH05MMBAA.jpg'
+    },
+    {
+      id: 3,
+      title: 'Pemrograman Web Seri Php: Langkah Mudah Dan Praktis Memahami',
+      description: 'Saat ini, PHP banyak dipakai untuk membuat program situs web dinamis. Contoh aplikasi program PHP adalah forum (phpBB) dan MediaWiki (software di belakang Wikipedia). ',
+      image: 'https://cdn.gramedia.com/uploads/items/Pemrograman_WEb_PHP.jpg'
+    },
+    {
+      id: 4,
+      title: 'Mahir Bahasa Pemrograman PHP',
+      description: 'Dengan pesatnya perkembangan teknologi saat ini, kita dituntut untuk bisa membuat dan mengembangkan sebuah sistem atau program berbasis web. Pembuatan sebuah sistem berbasis web bisa menggunakan berbagai macam bahasa pemrograman. Namun, pada umumnya orang menggunakan bahasa pemrograman PHP.',
+      image: 'https://cdn.gramedia.com/uploads/items/9786020498768_Mahir_Bahasa_.jpg'
+    }
+  ];
+
   return (
     <Container>
       <Row>
-        <BookComponent title="Warkop DKI Reborn" description="Warkop DKI Reborn Description" variant="primary" />
-        <BookComponent title="Warkop DKI Reborn 2" description="Warkop DKI Reborn Description" variant="primary" />
-        <BookComponent title="Warkop DKI Reborn 3" description="Warkop DKI Reborn Description" variant="primary" />
-        <BookComponent title="Warkop DKI Reborn 4" description="Warkop DKI Reborn Description" variant="primary" />
+        {
+          books.map((book) =>
+            <BookComponent
+              key={book.id}
+              title={book.title}
+              description={book.description}
+              image={book.image}
+              variant="primary" />
+          )
+        }
       </Row>
     </Container>
   );
 }
 
 export default App;
-
-/**
- * Component ada 2 (dua) jenis
- * 1. stateLess Component -> functional component
- * 2. stateFul Component -> class componnet
- */
